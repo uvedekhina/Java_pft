71fadca8e026462fb6e07ab7878d7463981f4b75
uvedekhina <55082086+uvedekhina@users.noreply.github.com>
2019-09-12 17:06:35 +0400

    созданны классы для квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+
+    hello("Julia");
+
+    Square s = new Square(5);
+
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(8,9);
+
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+  }
+
+  public static void hello(String smbd) {
+
+    System.out.println("Hello, " + smbd + "!");
+  }
+
+
+
+
+
+}
